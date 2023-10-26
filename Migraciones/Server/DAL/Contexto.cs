dfc69a0e34651bdec06407271ca36db5873770dc
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-10-26 12:10:06 -0400

    Añadiendo el contexto

---
+++ Migraciones/Server/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Migraciones.Shared.Models;
+
+namespace Migraciones.Server.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Prioridades> Prioridades { get; set; }
+    }
+}
